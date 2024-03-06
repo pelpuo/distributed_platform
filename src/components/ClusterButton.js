55589066a8d2b73fc3b57b9979a91a74c6149ab9
Edwin Kayang <epkayang@gmail.com>
2024-03-06 12:10:40 -0700

    initial commit

---
+++ src/components/ClusterButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ClusterButton() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ClusterButton
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'app-black':'#231F20',
+        'app-dark':'#262223',
+        'app-lighter-dark':'#292426',
+        'app-gray':'#949393',
+        'app-yellow':'#F8C542',
+        'app-red':'#761638',
+        'app-white':'#C7C7C7'
+      }
+    },
+  },
+  plugins: [],
+}
